44b26d315cadb2d9473574366a032afa51588c0a
GeoffGeis <geoffgeis@hotmail.com>
2014-10-17 01:27:47 -0500

    updated

--- tip_calculator.cpp
+++ tip_calculator.cpp
@@ -32,7 +32,17 @@ public:
     }
   }
 
-  float random_tip() {
-    output[] = 
+  int random_tip() {
+    float output[30] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10;
+                        11, 12, 13, 14, 15, 16, 17, 18;
+                        19, 20, 21, 22, 23, 24, 25, 26;
+                        27, 28, 29, 30};
+    int randindex = rand() % 31;
+    tip = output[randindex];
+    return 0;
+  }
+
+  int print_values() {
+    
   }
 }
